139f96ca31708cad34c1114fe3c722851b536dbf
imen76 <50248320+imen76@users.noreply.github.com>
2020-05-08 16:18:14 +0100

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -29,15 +29,11 @@ CREATE TABLE users (
 );
 EOF
 
-# Allow Read/Write for Owner
-#sudo chmod -R 0755 /var/www/html/
-
-# Create info.php for testing php processing
 #sudo echo "<?php phpinfo(); ?>" >> /var/www/html/info.php
 #sudo nano /var/www/html/info.php
 #sudo cp -r ProjetPart1LAMP/ /var/www/html/
 #scp -r ProjetPart1LAMP/ useradm@192.168.56.20:/home/useradm
 
-# Open localhost in the default browser
-#xdg-open "http://localhost/ProjetPart1LAMP/info.php"
+# Open login page
+xdg-open "http://localhost/ProjetImen/ProjetLogin/login.php"
 
